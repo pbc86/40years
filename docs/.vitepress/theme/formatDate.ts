c3a5bb2e317deb1f7a9f3891091ec942d22af99e
bnd007 <bdn007@yahoo.com>
2026-09-21 16:36:55 +0300

    update date format

--- docs/.vitepress/theme/formatDate.ts
+++ docs/.vitepress/theme/formatDate.ts
@@ -1,6 +1,4 @@
-const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-
 export function formatDate(value: unknown): string {
 	const [year, month, day] = String(value).slice(0, 10).split('-')
-	return `${day}/${monthNames[Number(month) - 1]}/${year}`
+	return `${day}/${month}/${year}`
 }
